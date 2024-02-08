61b9a2fa63dc4690995ea183ec7b01ac88da9deb
LidlKim <hestergrant12@gmail.com>
2024-02-08 19:00:40 +0000

    install axios

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
-//import 'bootstrap/dist/css/bootstrap.css';
+import axios from "axios";
+
 export default function Weather() {
   return (
     <body>
